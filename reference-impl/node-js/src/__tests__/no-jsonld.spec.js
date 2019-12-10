f6d03c047c6649c74a4de62f45d3a50bb2ed6876
Orie Steele <orie@ohs.io>
2019-12-09 18:03:48 -0600

    add non jsonld example

---
+++ reference-impl/node-js/src/__tests__/no-jsonld.spec.js
@@ -0,0 +1,108 @@
+const jose = require("jose");
+const base64url = require("base64url");
+
+const { exampleUnsigned } = require("../__fixtures__");
+
+describe("non-jsonld", () => {
+  it("sign verify", async () => {
+    delete exampleUnsigned["@context"];
+    const privateKeyJwk = {
+      crv: "Ed25519",
+      x: "D-5zI9uCYOAk_bN_QWD2XAQ_gIyHUh-6OY7nVk-Rg0g",
+      d: "zA65gfNF5g2CLKQnl8uRbGI2IRjJIE7PTZki7Qin9bw",
+      kty: "OKP"
+    };
+    const header = {
+      alg: "EdDSA"
+    };
+    const payload = exampleUnsigned;
+    payload.proof = {
+      type: "JoseNonLinkedDataSignature2020",
+      created: "2019-12-09T06:02:19Z",
+      verificationMethod:
+        "did:key:z6MkfXT3gnptvkda3fmLVKHdJrm8gUnmx3faSd1iVeCAxYtP",
+      proofPurpose: "assertionMethod"
+    };
+    const flat = jose.JWS.sign.flattened(
+      payload,
+      jose.JWK.asKey(privateKeyJwk),
+      header
+    );
+    const jws = `${flat.protected}..${flat.signature}`;
+    const signed = { ...payload, proof: { ...payload.proof, jws } };
+    expect(signed).toEqual({
+      type: "https://credentials.workday.com/docs/credential-schema.json",
+      modelVersion: "1.0",
+      id:
+        "did:work:MDP8AsFhHzhwUvGNuYkX7T;id=06e126d1-fa44-4882-a243-1e326fbe21db;version=1.0",
+      name: "EmailCredentialSchema",
+      author: "did:work:MDP8AsFhHzhwUvGNuYkX7T",
+      authored: "2018-01-01T00:00:00+00:00",
+      schema: {
+        $schema: "http://json-schema.org/draft-07/schema#",
+        description: "Email",
+        type: "object",
+        properties: {
+          emailAddress: {
+            type: "string",
+            format: "email"
+          }
+        },
+        required: ["emailAddress"],
+        additionalProperties: false
+      },
+      proof: {
+        type: "JoseNonLinkedDataSignature2020",
+        created: "2019-12-09T06:02:19Z",
+        verificationMethod:
+          "did:key:z6MkfXT3gnptvkda3fmLVKHdJrm8gUnmx3faSd1iVeCAxYtP",
+        proofPurpose: "assertionMethod",
+        jws:
+          "eyJhbGciOiJFZERTQSJ9..fTZ_g83Qda4PR4tzrg6jtG4BsjBVueuEN2A57_I5ID0uWcZJQmmK8E2MoMiRxpSbcGebqujC1BEUmFIOqXScBw"
+      }
+    });
+
+    let reconstructedPayload = { ...signed };
+    let [
+      encodedHeader,
+      encodedSignature
+    ] = reconstructedPayload.proof.jws.split("..");
+    delete reconstructedPayload.proof.jws;
+    const encodedPayload = base64url.encode(
+      JSON.stringify(reconstructedPayload)
+    );
+    const reconstructedJWS = `${encodedHeader}.${encodedPayload}.${encodedSignature}`;
+
+    const verified = jose.JWS.verify(
+      reconstructedJWS,
+      jose.JWK.asKey(privateKeyJwk)
+    );
+
+    expect(verified).toEqual({
+      type: "https://credentials.workday.com/docs/credential-schema.json",
+      modelVersion: "1.0",
+      id:
+        "did:work:MDP8AsFhHzhwUvGNuYkX7T;id=06e126d1-fa44-4882-a243-1e326fbe21db;version=1.0",
+      name: "EmailCredentialSchema",
+      author: "did:work:MDP8AsFhHzhwUvGNuYkX7T",
+      authored: "2018-01-01T00:00:00+00:00",
+      schema: {
+        $schema: "http://json-schema.org/draft-07/schema#",
+        description: "Email",
+        type: "object",
+        properties: { emailAddress: { type: "string", format: "email" } },
+        required: ["emailAddress"],
+        additionalProperties: false
+      },
+      proof: {
+        type: "JoseNonLinkedDataSignature2020",
+        created: "2019-12-09T06:02:19Z",
+        verificationMethod:
+          "did:key:z6MkfXT3gnptvkda3fmLVKHdJrm8gUnmx3faSd1iVeCAxYtP",
+        proofPurpose: "assertionMethod"
+      }
+    });
+
+    // in a client library we might reappend the jws, after verifiying it...
+  });
+});
