4d0b2153afdc68f42f6f06f8b078f08d82ba8b13
gabe <gcohen@squareup.com>
2023-05-02 16:41:28 -0700

    remove restrict refs

--- common.js
+++ common.js
@@ -99,63 +99,6 @@ require(["core/pubsubhub"], (respecEvents) => {
 
 });
 
-// Removes dfns that aren't referenced anywhere in the spec.
-// To ensure a definition appears in the Terminology section, use
-//  and link to it!
-// This is triggered by postProcess in the respec config.
-function restrictRefs(utils, content, url){
-
-  // Get set of ids internal dfns referenced in the spec body
-  const internalDfnLinks = document.querySelectorAll("a.internalDFN");
-
-  let internalDfnIds = new Set();
-  for (const dfnLink of internalDfnLinks) {
-    const dfnHref = dfnLink.href.split("#")[1];
-    internalDfnIds.add(dfnHref);
-  }
-
-  // Remove unused dfns from the termlist
-  const termlist = document.querySelector(".termlist");
-  const linkIdsInDfns = [];
-  for (const child of termlist.querySelectorAll("dfn")){
-    if (!internalDfnIds.has(child.id)){
-      let dt = child.closest("dt");
-      let dd = dt.nextElementSibling;
-
-      // Get internal links from dfns we're going to remove
-      //  because these show up in the dfn-panels later and then
-      //  trigger the local-refs-exist linter (see below)
-      const linksInDfn = dd.querySelectorAll("a.internalDFN");
-      for (link of linksInDfn) {
-        linkIdsInDfns.push(link.id);
-      }
-
-      termlist.removeChild(dt);
-      termlist.removeChild(dd);
-    }
-  }
-
-  // Remove unused dfns from the dfn-panels
-  //  (these are hidden, but still trigger the local-refs-exist linter)
-  //  (this seems like a hack, there's probably a better way to hook into respec
-  //   before it gets to this point)
-  const dfnPanels = document.querySelectorAll(".dfn-panel");
-  for (const panel of dfnPanels) {
-    if (!internalDfnIds.has(panel.querySelector(".self-link").href.split("#")[1])){
-      panel.parentNode.removeChild(panel);
-    }
-
-    // Remove references to dfns we removed which link to other dfns
-    const panelLinks = panel.querySelectorAll("li a");
-    for (const link of panelLinks) {
-      if (linkIdsInDfns.includes(link.href.split("#")[1])) {
-        link.parentNode.removeChild(link);
-      }
-    }
-  }
-
-}
-
 function _esc(s) {
   return s.replace(/&/g,'&amp;')
     .replace(/>/g,'&gt;')
